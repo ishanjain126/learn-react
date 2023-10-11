b7d209101fae09ece75e202365551c02fa927c96
Ishan Jain <ishanjain@Ishans-MacBook-Pro.local>
2023-10-12 05:28:10 +0530

    functional components, elements

--- App.js
+++ App.js
@@ -1,64 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/** Create the nested structure along with the siblings inside the react
- *
- *
- * <div>
- *  <div id="chid">
- *      <h1>I am the nested and sibling h1</h1>
- *      <h2>I am the nested and the sibling h2</h2>
- *  </div>
- * </div>
- *
- * ReactElement(Object) = HTML(Browser understands)
- *
- */
+// React element => It is basically an object => HTMLElement(render)
+/* NEVER BE USING THIS SYNTAX */
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" }, // This is the object where we can pass the attributes
+//   "This is the header (Child)"
+// );
 
-// In order to create the siblings we need to use the array of children
-// instead of just one child
+// React Element
 
-// This is the core of React. This is how react creates the element
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am the nested and sibling h1"),
-    React.createElement("h2", {}, "I am the nested and the sibling h2"),
-  ])
+// JSX => HTML like  or XML-like syntax
+// JSX code gets transpiled before it reaches the JS Engine
+
+// JSX => (transpiled using Babel) React.createElement => ReactElement - JS Object => HTMLElement(render)
+// const heading = <h1>This is the JSX heading</h1>;
+
+// console.log(heading); // Returns the same result as heading, an Object
+
+// React Component
+const Title = () => (
+  <h1 className="head" tabIndex={"5"}>
+    Ishan Jain
+  </h1>
 );
 
-console.log(parent);
+const titleElement = <h1>This is the title element</h1>;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
-
-/*
-
-// What is the use of the empty object in the below line?
-// This object is the place where we give the attribute to the tag.
-// For eg: If we want to give id to the header
-
-const header = React.createElement(
-  "h1",
-  { id: "header" },
-  // {},
-  "Welcome to react basics using the CDN"
+// Component Composition
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    {titleElement}
+    <h1>Welcome to React Course</h1>
+  </div>
 );
 
-console.log(header); // This is returning the object for you
-// This header (React.createElement) is basically returning us the object
-// It has something called as props.
-// Props are basically the attributes which we pass while creating the
-// element, while children is the third argument which we provide while
-// creating the same.
+//
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// React DOM is used in order to render the object (returned from the createElement) onto the browser
+const root = ReactDOM.createRoot(document.getElementById("root")); // When rendered on the DOM, then it becomes the HTML element
 
-//  Job of the function is to create the object, create the tag which we
-// want and then put it inside the root.
-root.render(header); // This is responsible to convert the passed object into the h1 tag and put it into the DOM
-
-
-*/
+root.render(<HeadingComponent />);
